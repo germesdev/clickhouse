4afa12dfa65ed0e68bd491577a7eb9ce58f61d49
Tim <truecarry@gmail.com>
2018-12-25 04:07:25 +0300

    Update result.go

--- result.go
+++ result.go
@@ -7,10 +7,8 @@ import (
 type result struct{}
 
 func (*result) LastInsertId() (int64, error) {
-	fmt.Printf("[NOTSUPPORTED] LastInsertId is not supported")
 	return 0, nil
 }
 func (*result) RowsAffected() (int64, error) {
-	fmt.Printf("[NOTSUPPORTED] RowsAffected is not supported")
 	return 0, nil
 }
